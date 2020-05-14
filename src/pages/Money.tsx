9daabcda59a835506f99943542dad18c59b819e6
luobin <1096422441@qq.com>
2020-05-14 22:11:02 +0800

    基本完成计算器

--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -70,7 +70,51 @@ const CategorySection = styled.section`
 `;
 
 const NumberPadSection = styled.section`
-
+  display: flex;
+  flex-direction: column;
+  > .output {
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 0 5px rgba(0,0,0,0.25);
+  }
+  > .pad {
+    > button {
+      float: left;
+      width: 25%;
+      height: 64px;
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+      &.zero {
+      width: 50%;
+      }
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+      &:nth-child(2),&:nth-child(5) {
+        background: #ededed;
+      }
+      &:nth-child(3),&:nth-child(6),&:nth-child(9) {
+        background: #d3d3d3;
+      }
+      &:nth-child(4),&:nth-child(7),&:nth-child(10) {
+        background: #c1c1c1;
+      }
+      &:nth-child(8),&:nth-child(11),&:nth-child(13) {
+        background: #b8b8b8;
+      }
+      &:nth-child(12) {
+        background: #a9a9a9;
+      }
+      &:nth-child(14) {
+        background: #9a9a9a;
+      }
+    }
+  }
 `;
 
 
@@ -99,21 +143,24 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>
+        <div className="output">
           100
         </div>
-        <div>
+        <div className="pad clearfix ">
           <button>1</button>
           <button>2</button>
           <button>3</button>
+          <button>删除</button>
           <button>4</button>
           <button>5</button>
           <button>6</button>
+          <button>清空</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>0</button>
-          <button>.</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
         </div>
       </NumberPadSection>
     </Layout>
