daa92925a5dc8002c5d3e9cdee0c5c3fc8705625
luobin <1096422441@qq.com>
2020-05-14 09:12:30 +0800

    Money组件完成TagsSection

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,35 @@ import Tags from './pages/Tags';
 import Money from './pages/Money';
 import Statistics from './pages/Statistics';
 import NoMatch from './pages/NoMatch';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/tags" exact>
-          <Tags />
-        </Route>
-        <Route path="/money" exact>
-          <Money />
-        </Route>
-        <Route path="/statistics" exact>
-          <Statistics />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/tags" />
-        </Route>
-        <Route path="*" exact>
-          <NoMatch/>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags" exact>
+            <Tags/>
+          </Route>
+          <Route path="/money" exact>
+            <Money/>
+          </Route>
+          <Route path="/statistics" exact>
+            <Statistics/>
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/tags"/>
+          </Route>
+          <Route path="*" exact>
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -4,6 +4,25 @@ import styled from 'styled-components';
 
 const TagSection = styled.section`
   background: #ffffff;
+  padding: 0 16px;
+  > ol {
+     margin: 0 -12px;
+     > li {
+      background: #d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 16px;
+      margin: 8px 12px;
+    }
+  }
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 10px;
+  }
 `;
 
 const NoteSection = styled.section`
@@ -18,24 +37,17 @@ const NumberPadSection = styled.section`
 
 `;
 
-const TagList = styled.ol`
-  > li {
-  background: #d9d9d9;
-  border-radius: 18px;
-  display: inline-block;
-  }
-`;
 
 function Money() {
   return (
     <Layout>
       <TagSection>
-        <TagList>
+        <ol>
           <li>衣</li>
           <li>食</li>
           <li>住</li>
           <li>行</li>
-        </TagList>
+        </ol>
         <button>新增标签</button>
       </TagSection>
       <NoteSection>
