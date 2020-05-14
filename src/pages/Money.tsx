a58472da0c08a14f84ed6b792d290942ea08e1ac
luobin <1096422441@qq.com>
2020-05-14 09:29:36 +0800

    Money组件Category ui完成

--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -46,7 +46,27 @@ const NoteSection = styled.section`
 `;
 
 const CategorySection = styled.section`
-
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+    > li {
+      width: 50%;
+      font-size: 24px;
+      padding: 16px 0;
+      text-align: center;
+      position: relative;
+      &.active::after {
+        content: '';
+        display: block;
+        height: 3px;
+        background: #333;
+        position: absolute;
+        width: 100%;
+        left: 0;
+        bottom: 0;
+      }
+    }
+  }
 `;
 
 const NumberPadSection = styled.section`
@@ -74,7 +94,7 @@ function Money() {
       </NoteSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="active">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
